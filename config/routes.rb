5eea5e22511ecca023721731c419af0db5f1d47a
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-12-06 12:20:04 -0500

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :coders
   root 'welcome#index'
   get 'welcome/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206160007) do
+ActiveRecord::Schema.define(version: 20151206171856) do
+
+  create_table "coders", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "coders", ["email"], name: "index_coders_on_email", unique: true
+  add_index "coders", ["reset_password_token"], name: "index_coders_on_reset_password_token", unique: true
 
   create_table "codes", force: :cascade do |t|
     t.string   "title"
