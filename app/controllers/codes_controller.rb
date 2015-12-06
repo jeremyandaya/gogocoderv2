f1d5ee7962f20364007606918eb22e6062157357
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-12-06 11:51:34 -0500

    added code views

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,14 +1,24 @@
 class CodesController < ApplicationController
+	before_action :find_code, only: [:show, :edit, :update, :destroy]
 	def index
+		@codes = Code.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@code = Code.new
 	end
 
 	def create
+		@code = Code.new(code_params)
+
+		if @code.save
+			redirect_to @code
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -23,8 +33,10 @@ class CodesController < ApplicationController
 	private
 
 		def find_code
+			@code = Code.find(params[:id])
 		end
 
 		def code_params
+			params.require(:code).permit(:title, :content)
 		end
 end
